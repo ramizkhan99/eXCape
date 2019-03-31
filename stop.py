30024445374b18323348fb2fff58d4894dec483b
BanerjeeRaunak99 <43110997+BanerjeeRaunak99@users.noreply.github.com>
2019-03-31 06:34:54 +0530

    Add files via upload

--- stop.py
+++ stop.py
@@ -1,10 +1,6 @@
-from pynput.keyboard import Key, Controller as KeyboardController
-from pynput.mouse import Button, Controller as MouseController
 import numpy as np
 import cv2
 
-keyboard = KeyboardController()
-mouse = MouseController()
 
 fist = cv2.CascadeClassifier('aGest.xml')
 
@@ -13,17 +9,12 @@ fist = cv2.CascadeClassifier('aGest.xml')
 cap = cv2.VideoCapture(0)
 
 while 1:
-    ret, img = cap.read()
+    ret, img1= cap.read()
+    img = cv2.flip(img1,1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     fists=fist.detectMultiScale(gray, 1.3, 5)
     if len(fists)!=0:
         print("stop")
-        mouse.position = (50, 70)
-        mouse.press(Button.left)
-        mouse.release(Button.left)
-        keyboard.press('s')
-        keyboard.release('s')
-
 
 
     cv2.imshow('img',img)
